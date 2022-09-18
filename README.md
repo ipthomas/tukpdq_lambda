386ed91f233f1a1d95a76f03a15d5e8e1b9b7af8
Ian <ian.thomas@tiani-spirit.com>
2022-09-18 17:26:55 +0100

    modified:   README.md modified:   build/main.zip modified:   main/main.go

--- README.md
+++ README.md
@@ -1,6 +1,36 @@
-# tuk_lambda_patping
+# tukpdq_lambda
 
-IHE PDQ Client implementation for deployment as an AWS Lambda function. It uses github.com/ipthomas/tukpdq package to perform either an :-
-    IHE PIXm Fhir patient query against an IHE PIXm compliant Server
-    IHE PIXv3 SOAP patient query against an IHE PIXv3 compliant Server
-    IHE PDQv3 SOAP patient query against an IHE PDQv3 compliant Server
+This is an IHE PDQ Client implementation for deployment in AWS as a Lambda function. 
+
+It uses the github.com/ipthomas/tukpdq package to perform an IHE PDQ against either :-
+    An IHE PIXm compliant Server using Fhir
+    An IHE PIXv3 compliant Server using SOAP
+    An IHE PDQv3 compliant Server using SOAP
+
+Required AWS Environment Variables are:
+    Key                                         (Example Values)
+    NHS_OID	                                    2.16.840.1.113883.2.1.4.1
+    REG_OID	                                    2.16.840.1.113883.2.1.3.31.2.1.1
+    SERVER_DEFAULT	                            SERVER_PIXV3
+    SERVER_PDQV3	                            http://spirit-test-01.tianispirit.co.uk:8081/SpiritPIX/PDQSupplier
+    SERVER_PIXM	                                http://spirit-test-01.tianispirit.co.uk:8081/SpiritPIXFhir/r4/Patient
+    SERVER_PIXV3	                            http://spirit-test-01.tianispirit.co.uk:8081/SpiritPIX/PIXManager
+
+It takes an AWS APIProxyRequest input and parses the requestParams:
+    If queryParam server is set, the value will overide the SERVER_DEFAULT Env var: Valid values are:
+        server=pixm
+        server=pixv3
+        server=pdqv3
+   
+    The PDQ client supports using either a MRN ID along with the MRN OID or a NHS ID or a Reg ID for the Patient ID to use in the query. If nhsoid or regoid queryParam values are provided they will override the env vars NHS_OID, REG_OID. 
+    The queryParams available for specifying the patient id are:
+        mrnid= and mrnoid=
+        nhsid= (and optional nhsoid=)
+        regid= (and optional regoid=)
+    
+    Patients are cached by default. To bypass the cache use the queryParam cache=false. The cache is valid as long as the state is maintained by the AWS lambda function.
+
+The queryParam rsptype specifies the required response to the PDQ. 
+    If queryParam rsptype="bool", the returned response is either 'true' or 'false'.
+    If queryParam rsptype="code", the http header 'statuscode' is set to either 200 if patient is found or 204 if not. The response body is empty.
+    The default returnes either the patient details or 'No Patient Found"
--- main/main.go
+++ main/main.go
@@ -18,51 +18,45 @@ func main() {
 	lambda.Start(Handle_Request)
 }
 func Handle_Request(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-	var server = "pixv3"
-	var nhsoid = "2.16.840.1.113883.2.1.4.1"
-	if os.Getenv("NHS_OID") != "" {
-		nhsoid = os.Getenv("NHS_OID")
-	}
-	if req.QueryStringParameters["nhsoid"] != "" {
-		nhsoid = req.QueryStringParameters["nhsoid"]
-	}
-	if os.Getenv("SERVER_DEFAULT") != "" {
-		server = strings.ToLower(strings.TrimPrefix(os.Getenv("SERVER_DEFAULT"), "SERVER_"))
-	}
-	if req.QueryStringParameters["server"] != "" {
-		server = strings.ToLower(req.QueryStringParameters["server"])
-	}
-	pdq := tukpdq.PDQQuery{
-		Server:     server,
-		MRN_ID:     req.QueryStringParameters["mrnid"],
-		MRN_OID:    req.QueryStringParameters["mrnoid"],
-		NHS_ID:     req.QueryStringParameters["nhsid"],
-		NHS_OID:    nhsoid,
-		REG_ID:     req.QueryStringParameters["regid"],
-		REG_OID:    os.Getenv("REG_OID"),
-		Server_URL: getServerURL(server),
-	}
-	setQueryPID(&pdq)
+	pdq := initPDQ(req)
 	if pdq.Used_PID == "" {
 		return handle_Response("", http.StatusBadRequest, errors.New("invalid request"))
 	}
-	if req.QueryStringParameters["cache"] == "true" {
+	if req.QueryStringParameters["cache"] != "false" {
 		if cachepat, ok := cachedpatients[pdq.Used_PID]; ok {
-			log.Printf("Cached ID %s found for registered patient", pdq.Used_PID)
+			log.Printf("Cached Patient found for Patient ID %s", pdq.Used_PID)
+			if req.QueryStringParameters["rsptype"] == "bool" {
+				return handle_Response("true", http.StatusOK, nil)
+			}
+			if req.QueryStringParameters["rsptype"] == "code" {
+				return handle_Response("", http.StatusOK, nil)
+			}
 			return handle_Response(string(cachepat), http.StatusOK, nil)
 		}
 	}
-	log.Printf("Using %s server for PDQ request", pdq.Server)
+	log.Printf("Using %s Server URL %s Patient ID %s for PDQ request", pdq.Server, pdq.Used_PID, pdq.Server_URL)
 	if err := tukpdq.PDQ(&pdq); err != nil {
 		return handle_Response("", pdq.StatusCode, err)
 	}
 	if pdq.Count < 1 {
-		return handle_Response("No Patient Found", http.StatusNoContent, nil)
+		if req.QueryStringParameters["rsptype"] == "bool" {
+			return handle_Response("false", http.StatusOK, nil)
+		}
+		if req.QueryStringParameters["rsptype"] == "code" {
+			return handle_Response("", http.StatusNoContent, nil)
+		}
+		return handle_Response("No Patient Found", http.StatusOK, nil)
 	}
 	if req.QueryStringParameters["cache"] == "true" {
 		cachedpatients[pdq.Used_PID] = pdq.Response
 	}
 	log.Printf("Patient ID %s is Registered", pdq.Used_PID)
+	if req.QueryStringParameters["rsptype"] == "bool" {
+		return handle_Response("true", http.StatusOK, nil)
+	}
+	if req.QueryStringParameters["rsptype"] == "code" {
+		return handle_Response("", http.StatusOK, nil)
+	}
 	return handle_Response(string(pdq.Response), http.StatusOK, nil)
 }
 func handle_Response(body string, status int, err error) (*events.APIGatewayProxyResponse, error) {
@@ -77,28 +71,53 @@ func handle_Response(body string, status int, err error) (*events.APIGatewayProx
 	}
 	return &resp, err
 }
-func getServerURL(server string) string {
+func initPDQ(req events.APIGatewayProxyRequest) tukpdq.PDQQuery {
+	var server = "pixv3"
+	var nhsoid = "2.16.840.1.113883.2.1.4.1"
+	var serverurl = ""
+	if os.Getenv("NHS_OID") != "" {
+		nhsoid = os.Getenv("NHS_OID")
+	}
+	if req.QueryStringParameters["nhsoid"] != "" {
+		nhsoid = req.QueryStringParameters["nhsoid"]
+	}
+	if os.Getenv("SERVER_DEFAULT") != "" {
+		server = strings.ToLower(strings.TrimPrefix(os.Getenv("SERVER_DEFAULT"), "SERVER_"))
+	}
+	if req.QueryStringParameters["server"] != "" {
+		server = strings.ToLower(req.QueryStringParameters["server"])
+	}
 	switch server {
 	case "pixm":
-		return os.Getenv("SERVER_PIXM")
+		serverurl = os.Getenv("SERVER_PIXM")
 	case "pdqv3":
-		return os.Getenv("SERVER_PDQV3")
+		serverurl = os.Getenv("SERVER_PDQV3")
+	case "pixv3":
+		serverurl = os.Getenv("SERVER_PIXV3")
 	}
-	return os.Getenv("SERVER_PIXV3")
-}
-func setQueryPID(i *tukpdq.PDQQuery) {
-	if i.MRN_ID != "" && i.MRN_OID != "" {
-		i.Used_PID = i.MRN_ID
-		i.Used_PID_OID = i.MRN_OID
+	pdq := tukpdq.PDQQuery{
+		Server:     server,
+		MRN_ID:     req.QueryStringParameters["mrnid"],
+		MRN_OID:    req.QueryStringParameters["mrnoid"],
+		NHS_ID:     req.QueryStringParameters["nhsid"],
+		NHS_OID:    nhsoid,
+		REG_ID:     req.QueryStringParameters["regid"],
+		REG_OID:    os.Getenv("REG_OID"),
+		Server_URL: serverurl,
+	}
+	if pdq.MRN_ID != "" && pdq.MRN_OID != "" {
+		pdq.Used_PID = pdq.MRN_ID
+		pdq.Used_PID_OID = pdq.MRN_OID
 	} else {
-		if i.NHS_ID != "" {
-			i.Used_PID = i.NHS_ID
-			i.Used_PID_OID = i.NHS_ID
+		if pdq.NHS_ID != "" {
+			pdq.Used_PID = pdq.NHS_ID
+			pdq.Used_PID_OID = pdq.NHS_ID
 		} else {
-			if i.REG_ID != "" && i.REG_OID != "" {
-				i.Used_PID = i.REG_ID
-				i.Used_PID_OID = i.REG_OID
+			if pdq.REG_ID != "" && pdq.REG_OID != "" {
+				pdq.Used_PID = pdq.REG_ID
+				pdq.Used_PID_OID = pdq.REG_OID
 			}
 		}
 	}
+	return pdq
 }
